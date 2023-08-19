5422f33d8374a7184f44d6c41b2444abdf46e1f7
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2023-08-19 10:57:44 +0700

    slacing chat item

--- src/components/molecules/ChatItem/Other.tsx
+++ src/components/molecules/ChatItem/Other.tsx
@@ -1,14 +1,18 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {colors, fonts} from '../../../utils';
+import {Image5} from '../../../assets';
 
 export default function Other() {
   return (
     <View style={styles.container}>
-      <View style={styles.content}>
-        <Text style={styles.text}>
-          Ibu dok, makanan yang sehat untuk membantu proses diet itu apa ?
-        </Text>
+      <Image source={Image5} style={styles.avatar} />
+      <View>
+        <View style={styles.content}>
+          <Text style={styles.text}>
+            Ibu dok, makanan yang sehat untuk membantu proses diet itu apa ?
+          </Text>
+        </View>
       </View>
       <Text style={styles.date}>4.20 AM</Text>
     </View>
@@ -19,26 +23,32 @@ const styles = StyleSheet.create({
   container: {
     marginBottom: 20,
     alignItems: 'flex-end',
-    paddingRight: 16,
+    paddingLeft: 16,
+    flexDirection: 'row',
   },
   content: {
     padding: 12,
     paddingRight: 18,
-    backgroundColor: colors.cardLight,
-    maxWidth: '70%',
-    borderTopRightRadius: 20,
-    borderTopLeftRadius: 20,
-    borderBottomLeftRadius: 20,
+    backgroundColor: colors.primary,
+    maxWidth: '80%',
+    borderRadius: 20,
+    borderBottomLeftRadius: 0,
   },
   text: {
     fontSize: 14,
     fontFamily: fonts.primary.normal,
-    color: colors.text.primary,
+    color: colors.white,
   },
   date: {
     fontSize: 11,
     fontFamily: fonts.primary.normal,
     color: colors.text.secondary,
     marginTop: 8,
+  },
+  avatar: {
+    width: 30,
+    height: 30,
+    borderRadius: 30 / 2,
+    marginRight: 12,
   },
 });
